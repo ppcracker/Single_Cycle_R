05ad6088c7623019ca6a224a429246a4b26b6bc4
Deepika <146109841+ppcracker@users.noreply.github.com>
2025-01-09 15:36:19 +0530

    Update Instruction_memory.v

--- Instruction_memory.v
+++ Instruction_memory.v
@@ -8,20 +8,20 @@ module Instruction_Memory(rst,A,RD);
   
   assign RD = (~rst) ? {32{1'b0}} : mem[A[31:2]];
 
-  initial begin
+ /* initial begin
     $readmemh("memfile.hex",mem);
-  end
+  end */
 
 
-/*
+
   initial begin
-    //mem[0] = 32'hFFC4A303;
-    //mem[1] = 32'h00832383;
-    // mem[0] = 32'h0064A423;
-    // mem[1] = 32'h00B62423;
+   // mem[0] = 32'hFFC4A303;
+  //mem[1] = 32'h00832383;
+  // mem[0] = 32'h0064A423;
+  // mem[1] = 32'h00B62423;
     mem[0] = 32'h0062E233;
-    // mem[1] = 32'h00B62423;
+    mem[1] = 32'h00B62423;
 
   end
-*/
+
 endmodule
